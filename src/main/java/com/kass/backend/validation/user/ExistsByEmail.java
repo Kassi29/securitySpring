58e45da90e45e0ad6f5bad08d039d751e07ff006
Kass Badani <kassbd29@gmail.com>
2024-09-17 11:17:45 -0400

    Mejoro el valid by email

--- src/main/java/com/kass/backend/validation/user/ExistsByEmail.java
+++ src/main/java/com/kass/backend/validation/user/ExistsByEmail.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//@Constraint(validatedBy = ExistsByEmailValidation.class)
+@Constraint(validatedBy = ExistsByEmailValidation.class)
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExistsByEmail {
--- src/main/java/com/kass/backend/validation/user/ExistsByEmailValidation.java
+++ src/main/java/com/kass/backend/validation/user/ExistsByEmailValidation.java
@@ -3,18 +3,20 @@ package com.kass.backend.validation.user;
 import com.kass.backend.services.UserService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ExistsByEmailValidation implements ConstraintValidator<ExistsByEmail, String> {
 
-    private final UserService userService;
-
-
-    public ExistsByEmailValidation(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @Override
-    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        if (email == null || userService == null) {
+            return true;
+        }
         return !userService.existsByEmail(email);
     }
 }
